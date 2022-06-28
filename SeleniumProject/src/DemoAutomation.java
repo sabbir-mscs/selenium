ce2724a297d4e08f13c6e98cab5a52f84784d6a7
SABBIR AHMED <sabbir.mscs@gmail.com>
2022-06-28 16:45:40 +0600

    WebDriver Installation

---
+++ SeleniumProject/src/DemoAutomation.java
@@ -0,0 +1,22 @@
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class DemoAutomation {
+
+	public static void main(String[] args) {
+		
+//		System.setProperty("webdriver.chrome.driver", "E:\\test_files\\selenium\\selenium-demo\\selenium-tools\\browserdrivers\\chromedriver.exe");
+//		System.setProperty("webdriver.gecko.driver", "E:\\test_files\\selenium\\selenium-demo\\selenium-tools\\browserdrivers\\geckodriver.exe");
+		System.setProperty("webdriver.edge.driver", "E:\\test_files\\selenium\\selenium-demo\\selenium-tools\\browserdrivers\\msedgedriver.exe");
+
+		
+		
+//		ChromeDriver driver = new ChromeDriver();
+//		FirefoxDriver driver = new FirefoxDriver();
+		EdgeDriver driver = new EdgeDriver();
+		
+		driver.get("https://google.com");
+	}
+
+}
