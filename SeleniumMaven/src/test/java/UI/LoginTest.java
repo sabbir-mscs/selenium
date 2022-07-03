6824f14ecc61d09a88b4fd42960673f7f188bbe1
SABBIR AHMED <sabbir.mscs@gmail.com>
2022-07-03 16:57:12 +0600

    Webdriver Interface implementation

--- SeleniumMaven/src/test/java/UI/LoginTest.java
+++ SeleniumMaven/src/test/java/UI/LoginTest.java
@@ -1,6 +1,7 @@
 package UI;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -8,21 +9,27 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
 
-	public static String browser = "Chrome";
-	public static FirefoxDriver driver;
-	public static ChromeDriver driver1;
+	public static String browser = "firefox";  //External configuration - XLS, CSV
+	public static WebDriver driver;
+//	public static FirefoxDriver driver;
+//	public static ChromeDriver driver1;
 	
 	public static void main(String[] args) { 
 		
-		if(browser.equals("Firefox"))
+		if(browser.equals("firefox"))
 		{
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 		}
-		else if(browser.equals("Chrome")) 
+		else if(browser.equals("chrome")) 
 		{
 			WebDriverManager.chromedriver().setup();
-			driver1 = new ChromeDriver();
+			driver = new ChromeDriver();
+		}
+		else if(browser.equals("edge")) 
+		{
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
 		}
 		
 //		WebDriverManager.chromedriver().setup();
@@ -32,7 +39,9 @@ public class LoginTest {
 		driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
 		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
 		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
-//		driver.close();
+		driver.close();
+		
+		
 		
 	}
 
