24b900c5db2afa596a7dadf46430344902f5001c
SABBIR AHMED <sabbir.mscs@gmail.com>
2022-08-03 13:31:06 +0600

    Basic WebDriver Methods 2

--- SeleniumMaven/src/test/java/ui/Webdriver_Interface_2.java
+++ SeleniumMaven/src/test/java/ui/Webdriver_Interface_2.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Webdriver_Interface {
+public class Webdriver_Interface_2 {
 
 	public static String browser = "firefox"; // External configuration - XLS, CSV
 	public static WebDriver driver;
@@ -25,7 +25,23 @@ public class Webdriver_Interface {
 			driver = new EdgeDriver();
 		}
 
-		driver.get("https://developer.salesforce.com/signup");
+		//get() 	
+		driver.get("https://saucedemo.com");
+		//manage()
+		driver.manage().window().maximize();
+		//getCurrentUrl()
+		String currenturl = driver.getCurrentUrl();
+		System.out.println(currenturl);
+		//getTitle()
+		driver.getTitle();
+		String title = driver.getTitle();
+		System.out.println(title);
+		//pageSource()
+		String pagesource = driver.getPageSource();
+		System.out.println(pagesource);
+		//navigate
+		driver.navigate().to("https://developer.salesforce.com/signup");
+		//quit() or close()
 		driver.close();
 
 	}
